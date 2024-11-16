9bc5aca3cbe15bccd1d858634d093d89826d0493
Yi-99 <ylim.8299@gmail.com>
2024-11-16 14:39:35 -0700

    fix: main fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< Updated upstream
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-=======
 import Pentagon from './component/Pentagon.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 		<App />
 		<Pentagon />
->>>>>>> Stashed changes
   </React.StrictMode>,
 )
