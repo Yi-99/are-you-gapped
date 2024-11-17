c220be681e7853273c5e8ddd25f85b151a0ec125
Yi-99 <ylim.8299@gmail.com>
2024-11-16 17:21:31 -0700

    feat: result finished

--- frontend/src/SkillAnalyzePage.jsx
+++ frontend/src/SkillAnalyzePage.jsx
@@ -4,6 +4,7 @@ import Header from './component/Header'
 import OverallScore from './component/OverallScore.jsx';
 import Pentagon from './component/Pentagon.jsx';
 import CareerProgress from './page/GapAnalyzer'
+import ChartDescription from './component/ChartDescription.jsx';
 import './SkillAnalyzePage.css'
 
 const SkillAnalyzePage = () => {
@@ -17,22 +18,24 @@ const SkillAnalyzePage = () => {
 				</div>
 			</div>
 			<div className="body flex flex-col p-4 m-4">
-				<p className="text-[#185440] text-4xl font-extrabold">Result & Feedback</p>
-				<p className="text-[#185440] text-start flex-wrap">This radar chart evaluates your skill and guides you through your career journey.</p>
-				<div className="green-br"></div>
-				<div className='body flex flex-row justify-around mb-16'>
-					<div className="radar-chart-container shadow-md p-4 m-4 flex flex-row justify-center items-center w-fit">
+				<div className="px-32 mx-32">
+					<p className="text-[#185440] text-4xl font-extrabold">Result & Feedback</p>
+					<p className="text-[#185440] text-start flex-wrap">This radar chart evaluates your skill and guides you through your career journey.</p>
+					<div className="green-br"></div>
+				</div>
+				<div className='body min-w-500px flex mt-16 flex-row justify-between mb-16 gap-x-24'>
+					<ChartDescription />
+					<div className="radar-chart-container mr-auto shadow-lg p-4 m-4 flex flex-row justify-center items-center w-fit">
 						<Pentagon cx={250} cy={250} outerRadius={125} width={500} height={500}/>
 					</div>
-					<div className="overall-score-container shadow-md p-8 m-8 mx-8 flex flex-col justify-center items-center w-fit">
-						<OverallScore />
-					</div>
+				</div>
+				
+				<div className='px-32 mx-32'>
+					<p className="text-[#185440] text-4xl mt-16 font-extrabold">We have a study plan for you!</p>
+					<p className="text-[#185440] text-start flex-wrap">This is a plan. you will meet with the advisor to further discuss the topics.</p>
+					<div className="green-br"></div>
 				</div>
 				<CareerProgress />
-				<p className="text-[#185440] text-4xl mt-16 font-extrabold">We have a study plan for you!</p>
-				<p className="text-[#185440] text-start flex-wrap">This is a plan. you will meet with the advisor to further discuss the topics.</p>
-				<div className="green-br"></div>
-				
 			</div>
 		</div>
 	)
